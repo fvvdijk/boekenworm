1ec1c8676c882524d32c4bb527855758bd8f4a8f
fvvdijk <f.v.v.dijk@protonmail.com>
2023-11-18 18:25:59 +0100

    Begin

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Searchbar() {
+    return (
+        <div>searchbar</div>
+    )
+}
+
+export default Searchbar;
+
+/* react componenten zijn te herkennen aan dat er in de return html staat en dat de
+functie altijd met een hoofdletter wordt geschreven */
